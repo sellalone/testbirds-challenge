bb7e15aa61557682d10d636778110cd43872da4c
sellalone <imsalehmuhammad@gmail.com>
2020-04-05 20:57:40 +0200

    Fix test

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -18,7 +18,7 @@ const App: SFC = () => {
     <Provider {...stores}>
       <Container>
         <Header />
-        <Router basename={`${process.env.PUBLIC_URL}`}>
+        <Router basename={`${process.env.PUBLIC_URL}/testbirds-challenge/`}>
           <Switch>
             <Route
               path="/"
